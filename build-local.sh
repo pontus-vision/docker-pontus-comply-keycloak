8c7d531678f25fc16a600d041179d1ca8fd08e0f
Leo Martins <lmartins@pontusvision.com>
2021-06-28 22:34:00 +0100

    wip

--- build-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-
-cd $DIR/docker
-docker build --rm . -t pontusvisiongdpr/pontus-comply-keycloak
-docker push pontusvisiongdpr/pontus-comply-keycloak
-
-
