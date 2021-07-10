bd1f81ec5327c2585343e580c278286506e6eac6
Leo Martins <lmartins@pontusvision.com>
2021-07-10 15:12:15 +0100

    wip

--- build-docker-lambda.sh
+++ build-docker-lambda.sh
@@ -28,7 +28,9 @@ else
   #docker tag pontus-comply-keycloak-lambda:${TAG} ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-comply-keycloak-lambda:${TAG}-${TIMESTAMP}  
   docker tag pontus-comply-keycloak-lambda:${TAG} ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-comply-keycloak-lambda:${TAG}
   docker push ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-comply-keycloak-lambda:${TAG}
-
+#  IMAGE_SHA=$(docker images --no-trunc --quiet ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-comply-keycloak-lambda:${TAG})  
+  IMAGE_SHA=$(aws ecr describe-images --repository-name pontus-comply-keycloak-lambda --image-ids imageTag=${TAG} | jq -r '.imageDetails[0].imageDigest')
+  aws lambda update-function-code --function-name keycloak  --image-uri 558029316991.dkr.ecr.eu-west-2.amazonaws.com/pontus-comply-keycloak-lambda@${IMAGE_SHA}
 fi
 rm KEYCLOAK_PASSWORD_FILE KEYCLOAK_USER_FILE
 
--- keycloak-lambda/pontus-docker-entrypoint.sh
+++ keycloak-lambda/pontus-docker-entrypoint.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 cd /mnt/keycloak 
+#export JAVA_OPTS=${JAVA_OPTS_OVERRIDE:-"-Djboss.http.port=8888"}
 
-if [[ (! -d /mnt/keycloak/keycloak-14.0.0)  || ("true" = "${PV_FORCE_REINSTALL}" ) ]]; then 
+if [[ (! -d /mnt/keycloak/update-keycloak-2021-07-10T1209)  || ("true" = "${PV_FORCE_REINSTALL}" ) ]]; then 
 
   echo "#####  REMOVING old folder"
-  rm -rf /mnt/keycloak/jboss || true
-  rm -rf /mnt/keycloak/keycloak-14.0.0 || true
+  rm -rf /mnt/keycloak/* || true
   echo "#####  CREATING new folder"
   tar xfz /opt/keycloak.tar.gz 
   echo "#####  Finished CREATING new folder"
+  mkdir /mnt/keycloak/update-keycloak-2021-07-10T1209
 #tar xfz /opt/jboss/keycloak/standalone.tar.gz
 #cp -a /opt/jboss/keycloak/standalone/* /mnt/keycloak/
-
 fi
-if [[ (! -d /mnt/keycloak/config_pv2)  || ("true" = "${PV_FORCE_RESET_ADMIN_USER}" ) ]]; then 
+if [[ (! -d /mnt/keycloak/config_pv05)  || ("true" = "${PV_FORCE_RESET_ADMIN_USER}" ) ]]; then 
    rm -rf /mnt/keycloak/config* || true
    #/mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -u admin -p Adminpa55word2@
-   /mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master -u admin -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   /mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master -u admin  -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@}
+   /mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master -u leo  -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@}
+   /mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r pontus -u leo  -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@}
+   #/mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh  -u admin1 -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   #/mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master --domain -u admin2 -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   #/mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master  -u admin3 -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   #/mnt/keycloak/keycloak-14.0.0/bin/add-user.sh -r master -u admin -g PowerUser,BillingAdmin -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   #/mnt/keycloak/keycloak-14.0.0/bin/add-user-keycloak.sh -r master -u leo --roles PowerUser,BillingAdmin -p ${PV_KEYCLOAK_ADMIN_PASSWD:-Adminpa55word2@!}
+   #/mnt/keycloak/keycloak-14.0.0/bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/keycloak.export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -Dkeycloak.profile.feature.upload_scripts=enabled &
    #/mnt/keycloak/keycloak-14.0.0/bin/add-user.sh -u "admin" -p "Adminpa55word2@!" -e -g PowerUser,BillingAdmin
-   mkdir /mnt/keycloak/config_pv2
+   mkdir /mnt/keycloak/config_pv05
+  /mnt/keycloak/keycloak-14.0.0/bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/opt/keycloak.export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -Dkeycloak.profile.feature.upload_scripts=enabled  -Djboss.http.port=8888 -Djboss.bind.address=127.0.0.1
+  exit 0
 fi
 #SECRETS=$(aws secretsmanager get-secret-value --secret-id test1/pv/admin | jq -r '.SecretString')
 #echo "AFTER GETTING SECRETS"
@@ -40,6 +50,6 @@ fi
 #export JBOSS_BASE_DIR=/mnt/keycloak/standalone
 echo "##### about to start keycloak"
 #export JAVA_OPTS=${JAVA_OPTS_OVERRIDE:-"-Djboss.http.port=8888 -Djboss.https.port=443"}
-export JAVA_OPTS=${JAVA_OPTS_OVERRIDE:-"-Djboss.http.port=8888"}
-cp /opt/standalone.xml /mnt/keycloak/keycloak-14.0.0/standalone/configuration/
-/mnt/keycloak/keycloak-14.0.0/bin/standalone.sh "$@"
+#cp /opt/standalone.xml /mnt/keycloak/keycloak-14.0.0/standalone/configuration/
+
+/mnt/keycloak/keycloak-14.0.0/bin/standalone.sh -Djboss.http.port=8888 -Djboss.bind.address=127.0.0.1
